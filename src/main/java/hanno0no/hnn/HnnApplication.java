5ec75740dfca2cc7dc2ef6f307b06ef265274ec6
sumin <emamgin@gmail.com>
2025-07-23 14:44:02 +0900

    initial commit

---
+++ src/main/java/hanno0no/hnn/HnnApplication.java
@@ -0,0 +1,13 @@
+package hanno0no.hnn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HnnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HnnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hanno0no/hnn/HnnApplicationTests.java
@@ -0,0 +1,13 @@
+package hanno0no.hnn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HnnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
